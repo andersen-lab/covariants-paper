bb674cc6d16c222c8a6b93b7d57db8d62a955ee5
dylanpilz <dylan.pilz@live.com>
2025-07-14 06:16:23 -0700

    set threads to 8

--- supp_fig1/scripts/benchmark_multithread.sh
+++ supp_fig1/scripts/benchmark_multithread.sh
@@ -3,6 +3,6 @@ set -ex
 declare -a read_counts=("1000" "10000" "100000")
 for read_count in "${read_counts[@]}"; do
     hyperfine --warmup 3 --runs 5 --export-json ../results/multi_thread_performance_${read_count}.json \
-        'SAM_Refiner -S ../bygul/jn.1.1_'${read_count}'_merged.trimmed.sam  -r ../NC_045512_Hu-1.fasta --min_count 1 --min_samp_abund 0.0 --ntcover 1 --wgs 1 --AAreport 1 --mp 4' \
-        'covar -i ../bygul/jn.1.1_'${read_count}'_merged.trimmed.bam -r ../NC_045512_Hu-1.fasta -a ../NC_045512_Hu-1.gff --min_depth 1 --min_quality 0 --threads 4'
+        'SAM_Refiner -S ../bygul/jn.1.1_'${read_count}'_merged.trimmed.sam  -r ../NC_045512_Hu-1.fasta --min_count 1 --min_samp_abund 0.0 --ntcover 1 --wgs 1 --AAreport 1 --mp 8' \
+        'covar -i ../bygul/jn.1.1_'${read_count}'_merged.trimmed.bam -r ../NC_045512_Hu-1.fasta -a ../NC_045512_Hu-1.gff --min_depth 1 --min_quality 0 --threads 8'
 done
